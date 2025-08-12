396ce9529f34bfb11d06e7726dab80da274f18c2
Murli Kumar <aimgodhere@gmail.com>
2025-08-12 21:19:09 +0530

    fixed register route bug

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -28,6 +28,10 @@ async function postRegisterController(req, res) {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
   res.cookie("token", token);
+  return res.status(201).json({
+    message: "User Registered Successfully.",
+    user: user,
+  });
 }
 
 module.exports = {
